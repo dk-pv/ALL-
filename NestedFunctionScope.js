2d02d136ec69e6e3ea794212a3ce19073449d4aa
dk <kukkudanish@gmail.com>
2025-09-26 16:22:21 +0530

    add

--- NestedFunctionScope.js
+++ NestedFunctionScope.js
@@ -1,10 +1,12 @@
-let a = 10 
-function outer (){
-    let b = 20
-    function inner (){
-        let c = 30
-        console.log(a, b, c)
-    }
-    inner()
+let a = 10;
+function outer() {
+  let b = 20;
+  function inner() {
+    let d= 30;
+      console.log(a, b, d)
+  }
+    inner();
 }
-outer()
+ 
+
+outer();
