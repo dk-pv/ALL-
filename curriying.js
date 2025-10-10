0b121b5544d05a8dc57772c0de516314e40018f6
dk <kukkudanish@gmail.com>
2025-10-10 17:55:09 +0530

    study this keyword

---
+++ curriying.js
@@ -0,0 +1,45 @@
+// function sum(a){
+//     return function(b){
+//         return function(c){
+//             return a+b+c
+//         }
+//     }
+
+// }
+// console.log(sum(5)(10)(15));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ thisKeyWord.js
@@ -0,0 +1,42 @@
+// const person = {
+//     get(){
+//         console.log(this)
+//     }
+// }
+// person.get()
+
+
+
+
+
+// const  person1 = {
+//     name : "Danish",
+//     age : 50,
+// }
+
+// function get(){
+//         console.log(`my name is ${this.name} and i'm ${this.age} years old !`)
+// }
+
+// get.call(person1)
+// get.apply(person1)
+
+// const nn = get.bind(person1)
+// nn()
+
+
+
+// "use strict"
+
+// function x(){
+//     console.log(this)
+// }
+// x()
+// Window.x()
+
+
+
+
+
+
+                        // Object in "this Keyword"
