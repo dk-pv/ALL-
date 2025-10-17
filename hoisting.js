655dd26a21eddfff5ff052feba96618d459838f3
dk <kukkudanish@gmail.com>
2025-10-17 14:34:22 +0530

    deep in to Hoisting

--- hoisting.js
+++ hoisting.js
@@ -1,6 +1,21 @@
-var x = 15
-function Get() {
-    console.log("good morning")
-}
-console.log(x);
-Get()
+// var x = 15
+// function Get() {
+//     console.log("good morning")
+// }
+// console.log(x);
+// Get()
+
+
+
+
+// var x = 10
+// function a() {
+//     function b() {
+//        function c() {
+//         console.log(x)
+//        } 
+//        c()
+//     }
+//     b()
+// }
+// a()
